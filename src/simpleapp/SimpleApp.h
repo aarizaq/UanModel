2da58a315046c85b99b901e3e92097d84e1068a2
Alfonso Ariza <aarizaq_m@hotmail.com>
2023-01-13 13:01:56 +0100

    Fix ifndef/define name

--- src/simpleapp/SimpleApp.h
+++ src/simpleapp/SimpleApp.h
@@ -13,8 +13,8 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
-#ifndef __LORA_OMNET_SIMPLELORAAPP_H_
-#define __LORA_OMNET_SIMPLELORAAPP_H_
+#ifndef __UAN_SIMPLEAPP_H_
+#define __UAN_SIMPLEAPP_H_
 
 #include <omnetpp.h>
 
